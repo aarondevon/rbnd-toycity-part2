7635fce12744b1aba8a5e617ef189fe5f36ea69c
Aaron Sawyer <me@aarondevon.com>
2016-04-04 11:06:31 -0500

    Add methods to print sales, products, and brands titles in ascii art

--- lib/app.rb
+++ lib/app.rb
@@ -1,8 +1,8 @@
+require 'json'
 def start
 
   # Grabs info from JSON file and sets up report file to export to.
   def setup_files
-  require 'json'
   path = File.join(File.dirname(__FILE__), '../data/products.json')
   file = File.read(path)
   # Made products_hash a global variable.
@@ -12,28 +12,59 @@ def start
   end
 
   def create_report
-  end
+    # Print today's date
+    def print_todays_date
+      t = Time.now
+      puts "#{t.month}/#{t.day}/#{t.year}"
+    end
+    # Print "Sales Report" in ascii art
+    def print_sales_title
+      puts "  _____       _             _____                       _"
+      puts " / ____|     | |           |  __ \\                     | |"
+      puts "| (___   __ _| | ___  ___  | |__) |___ _ __   ___  _ __| |_ "
+      puts " \\___ \\ / _` | |/ _ \\/ __| |  _  // _ \\ '_ \\ / _ \\| '__| __|"
+      puts " ____) | (_| | |  __/\\__ \\ | | \\ \\  __/ |_) | (_) | |  | |_"
+      puts "|_____/ \\__,_|_|\\___||___/ |_|  \\_\\___| .__/ \\___/|_|   \\__|"
+      puts "                                      | |                   "
+      puts "                                      |_|                   "
+    end
+    # Print "Products" in ascii art
+    def print_product_title
+      puts " _____               _            _"
+      puts "|  __ \\             | |          | |"
+      puts "| |__) | __ ___   __| |_   _  ___| |_ ___"
+      puts "|  ___/ '__/ _ \\ / _` | | | |/ __| __/ __|"
+      puts "| |   | | | (_) | (_| | |_| | (__| |_\\__ \\"
+      puts "|_|   |_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+    end
+    # For each product in the data set:
+    	# Print the name of the toy
+    	# Print the retail price of the toy
+    	# Calculate and print the total number of purchases
+    	# Calculate and print the total amount of sales
+    	# Calculate and print the average price the toy sold for
+    	# Calculate and print the average discount (% or $) based off the average sales price
 
+
+    # Print "Brands" in ascii art
+    def print_brands_title
+    puts "|  _ \\                    | |"
+    puts "| |_) |_ __ __ _ _ __   __| |___"
+    puts "|  _ <| '__/ _` | '_ \\ / _` / __|"
+    puts "| |_) | | | (_| | | | | (_| \\__ \\"
+    puts "|____/|_|  \\__,_|_| |_|\\__,_|___/"
+    end
+    # For each brand in the data set:
+    	# Print the name of the brand
+    	# Count and print the number of the brand's toys we stock
+    	# Calculate and print the average price of the brand's toys
+    	# Calculate and print the total sales volume of all the brand's toys combined
+
+      print_sales_title
+      print_product_title
+      print_brands_title
+  end
+  create_report
 end
 
-# Print "Sales Report" in ascii art
-
-# Print today's date
-
-# Print "Products" in ascii art
-
-# For each product in the data set:
-	# Print the name of the toy
-	# Print the retail price of the toy
-	# Calculate and print the total number of purchases
-	# Calculate and print the total amount of sales
-	# Calculate and print the average price the toy sold for
-	# Calculate and print the average discount (% or $) based off the average sales price
-
-# Print "Brands" in ascii art
-
-# For each brand in the data set:
-	# Print the name of the brand
-	# Count and print the number of the brand's toys we stock
-	# Calculate and print the average price of the brand's toys
-	# Calculate and print the total sales volume of all the brand's toys combined
+start
