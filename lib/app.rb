8c5df96f6b807dd63315e3bb5016005714ba2114
Aaron Sawyer <me@aarondevon.com>
2016-04-08 14:25:14 -0500

    brand name methods

--- lib/app.rb
+++ lib/app.rb
@@ -12,6 +12,7 @@ end
 
 def create_report
   print_product_data
+  print_brand_data
 end
 
 # Print today's date
@@ -158,9 +159,38 @@ end
   $report_file.puts "|____/|_|  \\__,_|_| |_|\\__,_|___/"
   end
 
+  def brand_data(brand)
+    print_brand_name(brand)
+
+  end
+
+  def print_brand_data
+    print_brands_title
+    $products_hash["items"].each do |brand|
+      $report_file.puts line if brand["stock"] > 0
+      $report_file.puts brand_data(brand)
+      $report_file.puts line if brand["stock"] > 0
+      $report_file.puts space if brand["stock"] > 0
+    end
+  end
+
   # For each brand in the data set:
-  	# Print the name of the brand
-  	# Count and print the number of the brand's toys we stock
+  # Print the name of the
+  def get_brand_name(brand)
+    brand["brand"]
+  end
+
+  def print_brand_name(brand)
+    if brand["stock"] > 0
+      $report_file.puts get_brand_name(brand)
+    end
+  end
+
+  # Count and print the number of the brand's toys we stock
+  def count_stock(brand)
+
+  end
+  
   	# Calculate and print the average price of the brand's toys
   	# Calculate and print the total sales volume of all the brand's toys combined
 
